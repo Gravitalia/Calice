2121bdc5bdc659df40251bd3bf6fbf137bff6c19
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-03-04 17:04:44 +0100

    New links ✨

--- pages/index.vue
+++ pages/index.vue
@@ -4,4 +4,14 @@
         <h2 class="text-white text-4xl font-bold">Help center</h2>
         <SearchBar />
     </div>
+    <br />
+
+    <div class="grid grid-cols-4 gap-4 ml-12">
+        <NuxtLink prefetch to="/account" class="w-72 bg-white hover:bg-gray-100 h-32 border shadow-md hover:shadow-lg" align="center">
+            <br />
+            <p class="text-lg font-semibold">Account</p>
+            <br />
+            <p>All information for your account!</p>
+        </NuxtLink>
+    </div>
 </template>
